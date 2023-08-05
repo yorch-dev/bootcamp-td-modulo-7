175c9ceb2accfdeea1cf374b74502dcf660c71cc
yorch-dev <jltm.dev@outlook.com>
2023-08-04 22:00:53 -0400

    parte 2 ok

--- laboratorio/admin.py
+++ laboratorio/admin.py
@@ -9,6 +9,19 @@ class DirectorGeneralAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre', 'laboratorio')
     list_display_links = ('id', 'nombre', 'laboratorio')
 
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'laboratorio', 'p_costo', 'p_venta')
+    list_filter = ('nombre', 'laboratorio')
+    list_display_links = ('nombre', 'laboratorio')
+
+    def f_fabricacion_year(self, obj):
+        return obj.f_fabricacion.year
+
+    f_fabricacion_year.short_description = 'F Fabricacion'
+
+    list_display = list(list_display)
+    list_display.insert(list_display.index('p_costo'), 'f_fabricacion_year')
+
 admin.site.register(Laboratorio, LaboratorioAdmin)
 admin.site.register(DirectorGeneral, DirectorGeneralAdmin)
-admin.site.register(Producto)
+admin.site.register(Producto, ProductoAdmin)
